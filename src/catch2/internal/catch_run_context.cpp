895d601e6b2d0045effd5618e1a2b8b94f2bfc23
Martin Hořeňovský <martin.horenovsky@gmail.com>
2025-07-16 19:23:51 +0200

    Cache config::abortAfter and config::shouldDebugBreak in RunContext

--- src/catch2/internal/catch_run_context.cpp
+++ src/catch2/internal/catch_run_context.cpp
@@ -171,7 +171,9 @@ namespace Catch {
         m_reporter(CATCH_MOVE(reporter)),
         m_lastAssertionInfo{ StringRef(), SourceLineInfo("",0), StringRef(), ResultDisposition::Normal },
         m_outputRedirect( makeOutputRedirect( m_reporter->getPreferences().shouldRedirectStdOut ) ),
-        m_includeSuccessfulResults( m_config->includeSuccessfulResults() || m_reporter->getPreferences().shouldReportAllAssertions )
+        m_abortAfterXFailedAssertions( m_config->abortAfter() ),
+        m_includeSuccessfulResults( m_config->includeSuccessfulResults() || m_reporter->getPreferences().shouldReportAllAssertions ),
+        m_shouldDebugBreak( m_config->shouldDebugBreak() )
     {
         getCurrentMutableContext().setResultCapture( this );
         m_reporter->testRunStarting(m_runInfo);
@@ -522,7 +524,7 @@ namespace Catch {
     }
 
     bool RunContext::aborting() const {
-        return m_totals.assertions.failed >= static_cast<std::size_t>(m_config->abortAfter());
+        return m_totals.assertions.failed >= m_abortAfterXFailedAssertions;
     }
 
     void RunContext::runCurrentTest() {
@@ -679,7 +681,7 @@ namespace Catch {
 
     void RunContext::populateReaction( AssertionReaction& reaction,
                                        bool has_normal_disposition ) {
-        reaction.shouldDebugBreak = m_config->shouldDebugBreak();
+        reaction.shouldDebugBreak = m_shouldDebugBreak;
         reaction.shouldThrow = aborting() || has_normal_disposition;
     }
 
