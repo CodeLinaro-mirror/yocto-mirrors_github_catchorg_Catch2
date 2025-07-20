632d9a67104b7eb79f4482f7bccf1f0cb858290c
Martin Hořeňovský <martin.horenovsky@gmail.com>
2025-07-20 21:40:18 +0200

    Skeleton for applying deprecation warnings

--- CMake/CatchConfigOptions.cmake
+++ CMake/CatchConfigOptions.cmake
@@ -44,6 +44,7 @@ set(_OverridableOptions
   "GETENV"
   "EXPERIMENTAL_STATIC_ANALYSIS_SUPPORT"
   "USE_BUILTIN_CONSTANT_P"
+  "DEPRECATED_ANNOTATIONS"
 )
 
 foreach(OptionName ${_OverridableOptions})
