d65b07064c4b6dd88191dc83c0f83ae53495af14
Martin Hořeňovský <martin.horenovsky@gmail.com>
2026-04-07 11:09:36 +0200

    Test combined branch check in AssertionHandler::complete()

--- src/catch2/internal/catch_assertion_handler.cpp
+++ src/catch2/internal/catch_assertion_handler.cpp
@@ -43,19 +43,20 @@ namespace Catch {
 
     void AssertionHandler::complete() {
         m_completed = true;
-        if( m_reaction.shouldDebugBreak ) {
+        if ( m_reaction.shouldDebugBreak | m_reaction.shouldThrow |
+             m_reaction.shouldSkip ) {
+            if ( m_reaction.shouldDebugBreak ) {
 
-            // If you find your debugger stopping you here then go one level up on the
-            // call-stack for the code that caused it (typically a failed assertion)
+                // If you find your debugger stopping you here then go one level
+                // up on the call-stack for the code that caused it (typically a
+                // failed assertion)
 
-            // (To go back to the test and change execution, jump over the throw, next)
-            CATCH_BREAK_INTO_DEBUGGER();
-        }
-        if (m_reaction.shouldThrow) {
-            throw_test_failure_exception();
-        }
-        if ( m_reaction.shouldSkip ) {
-            throw_test_skip_exception();
+                // (To go back to the test and change execution, jump over the
+                // throw, next)
+                CATCH_BREAK_INTO_DEBUGGER();
+            }
+            if ( m_reaction.shouldThrow ) { throw_test_failure_exception(); }
+            if ( m_reaction.shouldSkip ) { throw_test_skip_exception(); }
         }
     }
 
